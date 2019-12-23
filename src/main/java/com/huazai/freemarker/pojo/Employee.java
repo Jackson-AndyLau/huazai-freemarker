5201c5131a90f4b26c1c13be5352a0db36a7eb28
huazai <who.seek.me@java98k.vip>
2019-12-23 20:23:19 +0800

    新增员工信息实体

---
+++ src/main/java/com/huazai/freemarker/pojo/Employee.java
@@ -0,0 +1,59 @@
+package com.huazai.freemarker.pojo;
+
+/**
+ * 
+ * @author HuaZai
+ * @contact who.seek.me@java98k.vip
+ *          <ul>
+ * @description 员工实体
+ *              </ul>
+ * @className Employee
+ * @package com.huazai.freemarker.pojo
+ * @createdTime 2017年06月18日
+ *
+ * @version V1.0.0
+ */
+public class Employee {
+
+	/**
+	 * 员工ID
+	 */
+	private String empId;
+
+	/**
+	 * 员工姓名
+	 */
+	private String empName;
+
+	public String getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + "]";
+	}
+
+	public Employee() {
+		super();
+	}
+
+	public Employee(String empId, String empName) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+	}
+
+}
